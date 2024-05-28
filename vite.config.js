4ace6688f98c77893aafb268c248ae2867d8f770
Vania 102383 <vania.morais@ua.pt>
2024-05-28 16:59:28 +0100

    another one

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './src', // Defina a raiz do projeto como a pasta 'src'
-  base: '/ChefDefensor/', // Substitua pelo nome correto do seu repositório
-  build: {
-    outDir: '../dist', // Saída da build para a pasta 'dist'
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'three': ['three'],
-          'three-gltf-loader': ['three/examples/jsm/loaders/GLTFLoader.js']
-        }
-      }
-    }
-  }
-});
+    base: "/ChefDefensor/",
+})
