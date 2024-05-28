38af7948e55d55d01fff269b08634f63b235596a
Vania 102383 <vania.morais@ua.pt>
2024-05-28 16:02:55 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/ChefDefensor/',  // Substitua 'nome-do-repositorio' pelo nome do seu repositório no GitHub
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+        output: {
+          manualChunks: {
+            'three': ['three'],
+            'three-gltf-loader': ['three/examples/jsm/loaders/GLTFLoader.js']
+          }
+        }
+      }
   }
 });
