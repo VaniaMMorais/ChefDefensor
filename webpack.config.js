640088248af50e19bf89e19d3470fea15e553f05
Vania 102383 <vania.morais@ua.pt>
2024-05-28 11:00:28 +0100

    mais tentativa

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
@@ -26,4 +36,10 @@ module.exports = {
             template: './index.html',
         }),
     ],
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            three: path.resolve('./node_modules/three')
+        }
+    }
 };
