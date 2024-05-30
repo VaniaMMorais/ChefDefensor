40252e4571677942664f5f34ff41c465daeeb782
Vania 102383 <vania.morais@ua.pt>
2024-05-30 11:57:31 +0100

    Finalmente deployed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "/",
-    build: {
-      outDir: 'dist'
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: './index.js', // Ponto de entrada para o arquivo index.js
+        main: './main.js'    // Ponto de entrada para o arquivo main.js
+      }
     }
-})
+  }
+});
